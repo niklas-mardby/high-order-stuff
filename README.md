e2c3ff2efe60dadc50b34a3a611fb416c6144862
Niklas Mårdby <niklas.mardby@zocom.se>
2023-02-08 11:10:56 +0100

    Add filter and more readme

--- README.md
+++ README.md
@@ -9,10 +9,13 @@ A demo and discussion of higher order functions in JS/TS
 - arrow function
 - [callback function (MDN)](https://developer.mozilla.org/en-US/docs/Glossary/Callback_function)
 - function chaining
-- JS built-in hof: [forEach](), [map](), [reduce](), [filter]()
+- JS built-in hof: [forEach](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach), [map](), [reduce](), [filter](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter)
 - [setTimeout()](https://developer.mozilla.org/en-US/docs/Web/API/setTimeout)
 - [this (MDN)](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this#as_an_object_method)
 - Immediately Invoked Function Expression = [IIFE (MDN)](https://developer.mozilla.org/en-US/docs/Glossary/IIFE)
+
+## Honorable mentions
+- (Copy an array)[https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#copy_an_array] and (Shallow copy)[https://developer.mozilla.org/en-US/docs/Glossary/Shallow_copy]
 
 # Recommended reading
 - [First-class Function (MDN)](https://developer.mozilla.org/en-US/docs/Glossary/First-class_Function)
--- src/app.ts
+++ src/app.ts
@@ -29,3 +29,74 @@ console.log(dubble(5));
 const tripple = mathy(3);
 console.log(tripple(5));
 
+// i js kan vi göra
+
+/* const o = {
+    name: "Gugge",
+    age: 12,
+}
+o.hobby = "lift heavy things";
+
+o.doSomething = () => {
+    console.log(this.name);
+}*/
+
+// =========== forEach ===========
+
+const arr: number[] = [1, 2, 3, 4, 5];
+
+arr.forEach((n) => {
+    console.log(n);
+});
+
+arr.forEach((e, i, arr) => {
+    //console.log(e + arr[i + 1]);
+    console.log(e + (i === arr.length - 1 ? 0 : arr[i + 1]));
+
+});
+
+// =========== filter ===========
+
+const animals: string[] = ["gris", "hund", "katt", "elefant", "blåval", "myrslok"];
+
+const a: string[] = animals.filter((o) => {
+    // test som returnerar true eller false
+    return o.length > 4;
+});
+
+// filter nedan gör samma sak som filter ovan
+// const b: string[] = animals.filter(o => o.length > 4)
+
+console.log(a);
+
+// shallow or deep? kom ihåg hur js gör med referenser
+console.log("referens?");
+
+const gugge = [1, 2, 3];
+const bobbo = gugge;
+bobbo[0] = 12;
+console.log(gugge);
+
+let x = 12;
+let y = x;
+y += 12;
+console.log(x);
+
+class Pirate {
+    name: string;
+
+    constructor(n: string) {
+        this.name = n;
+    }
+}
+
+const pirates: Pirate[] = [new Pirate("John Smith"), new Pirate("Bill")];
+
+const ps: Pirate[] = pirates.filter((p) => {
+    return p.name.length > 4;
+});
+
+console.log(ps);
+
+// =========== map
+// =========== reduce
